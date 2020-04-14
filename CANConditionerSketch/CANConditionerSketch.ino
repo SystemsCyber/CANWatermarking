f8d57516608b6d272e988e91d26f9e4fb381ff22
jeremy-daily <jeremy-daily@users.noreply.github.com>
2020-04-14 14:33:54 -0600

    Update CANConditionerSketch.ino

--- CANConditionerSketch/CANConditionerSketch.ino
+++ CANConditionerSketch/CANConditionerSketch.ino
@@ -12,7 +12,8 @@ ATECCX08A atecc;
 byte init_vector[16];
 byte aes_key[16];
 byte encrypted_key[16];
-
+uint8_t message_for_cmac[16];
+    
 CAN_message_t vehicle_msg;
 CAN_message_t ecu_msg;
 
@@ -22,13 +23,14 @@ boolean GREEN_LED_state;
 boolean YELLOW_LED_state;
 boolean AMBER_LED_state;
 
-uint32_t TXcount;
 uint32_t vehicle_rx_count;
 uint32_t ecu_rx_count;
-boolean new_block = false;
+uint8_t sa_index;
+uint8_t gateway_sa;
 
 elapsedMillis vehicle_rx_timer;
 elapsedMillis ecu_rx_timer;
+elapsedMillis cmac_timer;
 
 void atecc_error_flash(){
   while (1){
@@ -87,23 +89,32 @@ void setup(void) {
   
   Serial.print("Config Zone: \t");
   if (atecc.configLockStatus) Serial.println("Locked");
-  else Serial.println("NOT Locked");
+  else {
+    Serial.println("NOT Locked");
+    atecc_error_flash();
+  }
 
   Serial.print("Data/OTP Zone: \t");
   if (atecc.dataOTPLockStatus) Serial.println("Locked");
-  else Serial.println("NOT Locked");
+  else {
+    Serial.println("NOT Locked");
+    atecc_error_flash();
+  }
 
+  // Data Slot 0 is where we store the private key
   Serial.print("Data Slot 0: \t");
   if (atecc.slot0LockStatus) Serial.println("Locked");
-  else Serial.println("NOT Locked");
-
+  else {
+    Serial.println("NOT Locked");
+    atecc_error_flash();
+  }
   // if everything is locked up, then configuration is complete, so let's print the public key
-  if (atecc.configLockStatus && atecc.dataOTPLockStatus && atecc.slot0LockStatus) 
-  {
-    if(atecc.generatePublicKey(0) == false)
-    {
+  if (atecc.configLockStatus && 
+      atecc.dataOTPLockStatus && 
+      atecc.slot0LockStatus) {
+    if(atecc.generatePublicKey(0) == false){
       Serial.println("Failure to generate this device's Public Key");
-      Serial.println();
+      atecc_error_flash();
     }
   }
   else
@@ -111,7 +122,8 @@ void setup(void) {
     Serial.println("ATECC not Locked. Please provision correctly.");
     atecc_error_flash(); // stall out forever
   }
-  
+
+  // Produce Random Nonce for Session Key and Init. Vector
   atecc.updateRandom32Bytes();
   memcpy(&aes_key[0],&atecc.random32Bytes[0],sizeof(aes_key));
   memcpy(&init_vector[0],&atecc.random32Bytes[sizeof(aes_key)],sizeof(init_vector));
@@ -125,8 +137,8 @@ void setup(void) {
 
   // Get Gateway Public Key
   
-  uint8_t gateway_sa = 37;
-  uint8_t sa_index = get_sa_index(gateway_sa);
+  gateway_sa = 37;
+  sa_index = get_sa_index(gateway_sa);
   bool received_public_key = false;
   elapsedMillis wait_timer;
   while (received_public_key == false){
@@ -167,10 +179,39 @@ void setup(void) {
   print_bytes(encrypted_key, sizeof(encrypted_key) );
 
   send_session_key(encrypted_key, init_vector, gateway_sa);
-  
-  vehicle_rx_count = 0;
-  ecu_rx_count = 0;
-
+  delay(5);
+  wait_timer = 0;
+  bool matching = false;
+  while (matching == false){
+    if (wait_timer >= 2000){
+      wait_timer = 0;
+      send_session_key(encrypted_key, init_vector, gateway_sa);
+    }
+    if (vehicle_can.read(vehicle_msg)){
+      //ecu_can.write(vehicle_msg);
+      int num_bytes = parseJ1939(vehicle_msg);
+      if (j1939_pgn == DM18_PGN){
+        Serial.println("Data Security Message Found.");
+        Serial.printf("PGN: %04X, SA: %02X, DA: %02X, DLC: %d, Data: ", j1939_pgn, j1939_sa, j1939_da, num_bytes);
+        print_bytes(j1939_data, num_bytes);
+        uint8_t msg_len = j1939_data[0];
+        uint8_t msg_type = j1939_data[1];
+        if (msg_type == DM18_CONFIRMATION_TYPE){
+          uint8_t message[16];
+          memcpy(&message[0],&j1939_data[2],sizeof(message));
+          atecc.AES_ECB_decrypt(message);
+          Serial.println("Confirming Key Exchange. The following should match:");
+          print_bytes(atecc.AES_buffer,10);
+          print_bytes(init_vector,10);
+          matching = !memcmp(atecc.AES_buffer,init_vector,10);
+          if (matching) break;
+        }
+      }
+    } // end vehicle read message
+    if ( ecu_can.read(ecu_msg) ) {
+      vehicle_can.write(ecu_msg);
+    }
+  }
 }
 
 void loop() {
@@ -181,9 +222,7 @@ void loop() {
     ecu_can.write(vehicle_msg);
     AMBER_LED_state = !AMBER_LED_state;
     int num_bytes = parseJ1939(vehicle_msg);
-    Serial.println(num_bytes);
     if (num_bytes > 0){
-      int sa_index = get_sa_index(sa);
       if (num_bytes > 8){
         Serial.println("RX transport message:");
       }
@@ -215,6 +254,39 @@ void loop() {
   if ( ecu_can.read(ecu_msg) ) {
     ecu_rx_timer = 0;
     ecu_rx_count++;
+    update_cmac(ecu_msg);
     vehicle_can.write(ecu_msg);
   }
+  if (cmac_timer >= 2000){
+    cmac_timer = 0;
+    //Make a copy to produce an intermediate result
+    memcpy(&cmac_copy[sa_index],&cmac[sa_index],sizeof(cmac[sa_index]));
+    memcpy(&omac_copy[sa_index],&omac[sa_index],sizeof(omac[sa_index]));
+    Serial.println("CMAC Copy Finalize:");
+    cmac_copy[sa_index].finalize(omac_copy[sa_index]);
+    print_bytes(omac_copy[sa_index],sizeof(omac_copy[sa_index]));
+    vehicle_msg.id = 0x18D40000; //DM18 Message with normal priority
+    vehicle_msg.id += gateway_sa << 8;
+    vehicle_msg.id += SELF_SOURCE_ADDR;
+    vehicle_msg.len = 8;
+    vehicle_msg.buf[0] = 6; //Length
+    vehicle_msg.buf[1] = DM18_CMAC_TYPE;
+    memcpy(&vehicle_msg.buf[2],omac_copy[sa_index],6);
+    vehicle_can.write(vehicle_msg);
+    Serial.println("Sent 6 bytes of OMAC: ");
+    print_bytes(vehicle_msg.buf,6);
+    update_cmac(vehicle_msg); // update with messages generate
+  }
+
+  digitalWrite(RED_LED,RED_LED_state);
+  digitalWrite(GREEN_LED,GREEN_LED_state);
+  digitalWrite(YELLOW_LED,YELLOW_LED_state);
+  digitalWrite(AMBER_LED,AMBER_LED_state);
+}
+void update_cmac(CAN_message_t msg){
+  memset(message_for_cmac,0,sizeof(message_for_cmac));
+  memcpy(&message_for_cmac[0],&msg.id,4);
+  message_for_cmac[4] = msg.len;
+  memcpy(&message_for_cmac[5],&msg.buf[0],msg.len);
+  cmac[sa_index].update(omac[sa_index],message_for_cmac,sizeof(message_for_cmac));
 }
