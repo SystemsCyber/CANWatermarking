d6152f2e36d30935e66d4a5ba94988903614cd8c
Dr-Daily <56745553+Dr-Daily@users.noreply.github.com>
2022-04-26 09:36:07 -0600

    Update key.py

--- serverless/key.py
+++ serverless/key.py
@@ -2,10 +2,12 @@ import json
 import base64
 import random
 import string
+import os
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
 
 import boto3
 from botocore.exceptions import ClientError
@@ -14,6 +16,9 @@ from boto3.dynamodb.conditions import Key, Attr
 from utils import lambdaResponse as response
 
 region = 'us-east-1'
+
+os.environ['AdminSecretKey']= 'lRz4eNBQbObc5absat+To6u/keRlj8/6IbQAu6h9'
+os.environ['AdminAccessKeyId']= 'AKIARNA4VTG7D7GXMJJR'
 
 def get_key(event,context):
     """
@@ -96,7 +101,10 @@ def shared_secret(event,context):
                                 encoding = serialization.Encoding.PEM,
                                 format = serialization.PrivateFormat.PKCS8,
                                 encryption_algorithm = serialization.NoEncryption())
-    device_pub_key = base64.b64decode(key_dict['device_pub_key'])
+    device_pub_key_bytes = base64.b64decode(key_dict['device_pub_key'])
+    device_bytes = b'\x04' + device_pub_key_bytes #This makes DER keys
+    device_pub_key = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256R1(),
+                                                                      device_bytes)
     shared_secret = base64.b64encode(server_private_key.exchange(ec.ECDH(),device_pub_key)[:16]).decode('ascii')
 
     return response(200, {'server_pem_key':server_pem_key.decode('ascii'), 
@@ -137,7 +145,10 @@ def decrypt_data_key(data_key_encrypted):
     """
 
     # Decrypt the data key
-    kms_client = boto3.client('kms')
+    kms_client = boto3.client('kms', 
+                              region_name='us-east-1',
+                              aws_access_key_id = os.environ['AdminAccessKeyId'],
+                              aws_secret_access_key = os.environ['AdminSecretKey'])
     try:
         response = kms_client.decrypt(CiphertextBlob=data_key_encrypted)
     except ClientError as e:
@@ -149,15 +160,18 @@ def decrypt_data_key(data_key_encrypted):
 
 def decrypt_device_key(serial_number, admin_user=False):
     
-    dbClient = boto3.resource('dynamodb', region_name='us-east-1')
+    dbClient = boto3.resource('dynamodb', 
+                              region_name='us-east-1',
+                              aws_access_key_id = os.environ['AdminAccessKeyId'],
+                              aws_secret_access_key = os.environ['AdminSecretKey'])
     table = dbClient.Table("CANConditioners")
-    try:
-        item = table.get_item( 
+    #try:
+    item = table.get_item( 
             Key = {'id': serial_number,} 
         ).get('Item')
-    except:
-        print("Unable to retrieve serial number from table.")
-        return
+    # except:
+    #     print("Unable to retrieve serial number from table.")
+    #     return
 
     if item is None:
         print("Unable to retrieve serial number from table.")
@@ -189,6 +203,7 @@ def decrypt_device_key(serial_number, admin_user=False):
         'server_private_key': server_pem_key_pass.decode('ascii'),
         'device_password': device_password, #we may want to e-mail this out.
         'device_code': device_code, 
+        'server_pem_key_pass': server_pem_key_pass
         
     }
     if admin_user == True:
@@ -239,3 +254,65 @@ def roll_device_key_password(serial_number):
         print(e)
         return False
     return(True)
+
+if __name__ == '__main__':
+    ids= ["0123308F0B3BD236EE",
+          "01232701F2E7A649EE",
+          "012395F827E2BED6EE",
+          "0123C1B81F68E4C9EE",
+          "0123E81CF6720C1CEE",
+          "01231017636F64B4EE",
+          "0123364B5F74E501EE",
+          "01239F151E138F42EE",
+          "0123B334337CD440EE",
+          "01231346759CD71BEE",
+          "0123B9753679B913EE",
+          "0123DC96321F7FAEEE",
+          "012374B9754CBE4EEE",
+          "0123716DB252F46CEE",
+          "012362826474A4F4EE",
+          "0123B75EF1F75703EE",
+          "01233F098FD2694AEE",
+          "0123982154E9289EEE",
+          "01237C34D3A31BBAEE",
+          "01236494726CDF59EE",
+          "01237482C2910B18EE",
+          "012304D2E9253891EE",
+          "0123345D586AD20EEE"]
+    results = []
+    for sn in ids:
+        key_dict = decrypt_device_key(sn, admin_user=True)
+        print(key_dict)
+        server_pem_key_pass = key_dict['server_pem_key_pass']
+        full_password =  base64.b64decode(key_dict['full_password'])
+        server_private_key = serialization.load_pem_private_key(server_pem_key_pass, 
+                                                            password=full_password, 
+                                                            backend=default_backend())
+    
+        #Serialize server private key with password from full_password
+        new_server_pem_key_pass = server_private_key.private_bytes(
+                                encoding = serialization.Encoding.PEM,
+                                format = serialization.PrivateFormat.PKCS8,
+                                encryption_algorithm = serialization.NoEncryption())
+    
+        key_dict['server_pem_key_pass'] = new_server_pem_key_pass.decode('ascii')
+        #Serialize server private key with password from full_password
+        device_pub_key_bytes = base64.b64decode(key_dict['device_pub_key'])
+        #device_pub_key_bytes = bytes(bytearray.fromhex(pub_key.decode('utf-8')))
+        assert len(device_pub_key_bytes) == 64
+        key_dict['device_pub_key_ascii'] = "".join(["{:02X}".format(b) for b in device_pub_key_bytes])
+        device_bytes = b'\x04' + device_pub_key_bytes #This makes is a DER keys
+        device_pub_key = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256R1(),
+                                                                      device_bytes)
+
+        shared_secret = base64.b64encode(server_private_key.exchange(ec.ECDH(),device_pub_key)[:16]).decode('ascii')
+
+        device_pub_key_pem = device_pub_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        key_dict['device_pub_key_pem'] = device_pub_key_pem.decode('ascii')
+        key_dict['shared_secret'] = shared_secret
+        results.append(key_dict)
+
+    with open("key_file.json", 'w') as f:
+        json.dump(results,f, sort_keys=True, indent=4)
